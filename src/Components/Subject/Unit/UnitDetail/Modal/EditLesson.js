d203bfd98283f5ac494815f003677c356c605473
VnVentX <51308208+VnVentX@users.noreply.github.com>
2021-03-29 10:42:46 +0700

    Update Lesson Edit

--- src/Components/Subject/Unit/UnitDetail/Modal/EditLesson.js
+++ src/Components/Subject/Unit/UnitDetail/Modal/EditLesson.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal, Form, Input, Tooltip } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { EditOutlined } from "@ant-design/icons";
 
 const layout = {
   labelCol: { span: 6 },
   wrapperCol: { span: 18 },
 };
 
-const AddNewLesson = () => {
+const EditLesson = (props) => {
   const [form] = Form.useForm();
   const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    form.setFieldsValue({
+      lesson: props.data.title,
+      url: props.data.ppUrl,
+    });
+  }, []);
 
   const showModal = () => {
     setVisible(true);
@@ -28,8 +35,8 @@ const AddNewLesson = () => {
 
   return (
     <div>
-      <Tooltip title="Add Lesson">
-        <Button type="primary" icon={<PlusOutlined />} onClick={showModal} />
+      <Tooltip title="Edit Lesson">
+        <EditOutlined onClick={showModal} style={{ marginLeft: 10 }} />
       </Tooltip>
       <Modal
         title="Create New Lesson"
@@ -50,7 +57,7 @@ const AddNewLesson = () => {
       >
         <Form {...layout} form={form}>
           <Form.Item
-            name="subject"
+            name="lesson"
             label="Lesson Name"
             rules={[{ required: true, message: "Please input a subject name" }]}
           >
@@ -69,4 +76,4 @@ const AddNewLesson = () => {
   );
 };
 
-export default AddNewLesson;
+export default EditLesson;
--- src/Components/Subject/Unit/UnitDetail/UnitDetailComponent.js
+++ src/Components/Subject/Unit/UnitDetail/UnitDetailComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Tabs } from "antd";
 import Lesson from "./Lesson";
 import AddNewLesson from "./Modal/AddNewLesson";
+import EditLesson from "./Modal/EditLesson";
 
 const { TabPane } = Tabs;
 
@@ -9,10 +10,12 @@ const data = [
   {
     id: 1,
     title: "Lesson 1",
+    ppUrl: "",
   },
   {
     id: 2,
     title: "Lesson 2",
+    ppUrl: "",
   },
 ];
 
@@ -26,7 +29,15 @@ const UnitComponent = () => {
   return (
     <Tabs tabBarExtraContent={<AddNewLesson />}>
       {lesson?.map((i, idx) => (
-        <TabPane tab={i.title} key={idx + 1}>
+        <TabPane
+          tab={
+            <div style={{ display: "flex" }}>
+              {i.title}
+              <EditLesson data={i} />
+            </div>
+          }
+          key={idx + 1}
+        >
           <Lesson lesson={i.id} />
         </TabPane>
       ))}
